ae4294d27d15be5c1dbeba7d1d591ca71ae1c452
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-12-09 11:30:55 +0300

    Update and rename etk_tool.sh to es-theme-knulli-updater.sh

--- es-theme-knulli-updater.sh
+++ es-theme-knulli-updater.sh
@@ -4,7 +4,7 @@ version="1.0.0"
 
 script_dir="$(dirname "$(realpath "$0")")"
 
-curl -L https://raw.githubusercontent.com/symbuzzer/etk_tool/refs/heads/main/etk_tool.sh -o "$script_dir/etk_tool.sh"
+curl -L https://raw.githubusercontent.com/symbuzzer/etk_tool/refs/heads/main/es-theme-knulli-updater.sh -o "$script_dir/es-theme-knulli-updater.sh"
 
 if [ ! -f "$script_dir/etk_tool.pygame" ]; then
     curl -L https://raw.githubusercontent.com/symbuzzer/etk_tool/refs/heads/main/etk_tool.pygame -o "$script_dir/etk_tool.pygame"
